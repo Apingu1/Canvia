5b48162746f6b2380bf2b3a27f1107afa74f7c6d
Apingu1 <mumar1@live.co.uk>
2026-06-15 00:03:24 +0100

    Move Medesk links into config

---
+++ src/config/links.ts
@@ -0,0 +1,3 @@
+export const MEDESK_PATIENT_PORTAL_URL = "https://canvia.medesk.example/patient-login";
+export const MEDESK_BOOKING_URL = "https://canvia.medesk.example/book";
+export const MEDESK_CLINICIAN_LOGIN_URL = "https://canvia.medesk.example/clinician-login";
